86ff3f30fee00f454bff28d664963c4d7c50ec30
Jamie Benjamin <bennythedev@gmail.com>
2022-06-07 00:57:59 -0500

    commit tree 85e4766c897d99dfcbc9eed3ebdfb1d051d427ae

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    let hq = 42;
+    return Math.abs(pickupLocation - hq);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let destinationInFeet = destination * 264;
+    let startInFeet = start *264;
+    return Math.abs(destinationInFeet - startInFeet);
+}
+
+function calculatesFarePrice(start, destination) {
+    let message
+    let destinationInFeet = destination * 264;
+    let startInFeet = start *264;
+    let feetTravelled = Math.abs(destinationInFeet - startInFeet)
+    if (feetTravelled <=400) {
+        message = 0;
+    } else if (feetTravelled > 400 && feetTravelled < 2000) {
+        message = 2.56;
+    } else if (feetTravelled > 2000 && feetTravelled < 2500) {
+        message = 25;
+    } else if (feetTravelled > 2500) {
+        message = 'cannot travel that far';
+    }
+    return message
+}
